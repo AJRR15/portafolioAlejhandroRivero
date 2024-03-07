24ecce441184c6e8f97e54eda681a6dff801ba94
AJRR15 <arivero54409@ufide.ac.cr>
2024-03-06 20:38:24 -0600

    Elaboracion de CRUD Semana 8

--- tienda_AlejhandroR/src/main/java/tienda/demo/service/CategoriaService.java
+++ tienda_AlejhandroR/src/main/java/tienda/demo/service/CategoriaService.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -10,4 +10,7 @@ import java.util.List;
 public interface CategoriaService {    
     // Se obtiene un listado de categorias en un List   
     public List<Categoria> getCategorias(boolean activos); 
+    public Categoria getCategoria(Categoria categoria);
+    public void save (Categoria categoria);
+    public void delete (Categoria categoria);
 }
